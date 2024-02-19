dbaa9cad4dbe7c13d739683fb8197ac62693b137
Yihun <yankure01@gmail.com>
2024-02-19 13:35:38 +0300

    fdb-01

---
+++ lib/data/models/song_model.dart
@@ -0,0 +1,13 @@
+class Song {
+  final String id;
+  final String artist;
+  final String title;
+  final String url;
+
+  Song({
+    required this.id,
+    required this.artist,
+    required this.title,
+    required this.url,
+  });
+}
